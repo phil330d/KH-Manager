7eceea8c06a3e14a8d5add3cd9ef8eb82405ed2b
Theurer <5129@htl.rennweg.at>
2018-05-28 22:04:34 +0200

    working 1.0

--- KH-Manager.user.js
+++ KH-Manager.user.js
@@ -3,37 +3,30 @@
 // @version      1.0
 // @description  Filtert alle nicht geeigneten Krankenhäuser
 // @author       phil330d
-// @match        http://leitstellenspiel.de/*
-// @include *://www.leitstellenspiel.de/
-// @include *://www.leitstellenspiel.de/*
+// @include      *://www.leitstellenspiel.de/
+// @include      *://www.leitstellenspiel.de/*
 // @grant        none
+//@run           document-start
 // ==/UserScript==
 
 
 //TODO WENN EIGENES KRANKENHAUS -> KEINE PROZENTZAHl!
 
 
-(function (I18n, $) {
+(function($) {
     'use strict';
-
     function filter() {
-        $('.col-md-9 tbody > tr').each(function () {
+        $('.col-md-9 tbody > tr').each(function() {
             var el = $(this);
             var beds = parseInt(el.find('td:nth-child(3)').text().trim());
-            var percentage = el.find('td:nth-child(4)').text().trim();
-            // The column differs in owned and alliance table
-
-            console.log(beds);
-
-            console.log(percentage);
+            var percent = el.find('td:nth-child(4)').text.trim();
 
             var elegible = ((el.find(':nth-child(4)').text()
-                .trim().indexOf('10 %') === -1) ? el.find(
-                'td:nth-child(4)').text() : el.find(
-                ':nth-child(5)').text()).trim() === "Ja";
-            if ((beds <= 0) || (!elegible)) {
+                    .trim().indexOf('%') === -1) ? el.find(
+                        'td:nth-child(4)').text() : el.find(
+                        ':nth-child(5)').text()).trim() === "Ja";
+            if (beds < 1 || !elegible) {
                 el.fadeOut(1);
-                console.log("hallo");
             } else {
                 el.show();
             }
@@ -41,7 +34,8 @@
     }
 
     var sprechwunsch = $('#h2_sprechwunsch');
-    if (sprechwunsch.length > 0 && sprechwunsch.parent().text().indexOf("Krankenhaus") >= 0) {
+    if (sprechwunsch.length > 0 && sprechwunsch.parent().text().indexOf("Krankenhaus") > 0) {
         filter();
     }
-});
+
+})(jQuery);
