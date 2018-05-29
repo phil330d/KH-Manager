e19a08b016146676c77ac2572d6c60c0d1ad6dde
phil330d <34169039+phil330d@users.noreply.github.com>
2018-05-29 11:08:58 +0200

    Update KH-Manager-with-10%max.user.js

--- KH-Manager-with-10%max.user.js
+++ KH-Manager-with-10%max.user.js
@@ -3,43 +3,40 @@
 // @version      1.0
 // @description  Filtert alle nicht geeigneten Krankenhäuser
 // @author       phil330d
-// @match        http://leitstellenspiel.de/*
-// @include *://www.leitstellenspiel.de/
-// @include *://www.leitstellenspiel.de/*
+// @include      *://www.leitstellenspiel.de/
+// @include      *://www.leitstellenspiel.de/*
 // @grant        none
+// @run           document-start
 // ==/UserScript==
 
 
 (function ($) {
     'use strict';
-
     function filter() {
         $('.col-md-9 tbody > tr').each(function () {
             var el = $(this);
             var beds = parseInt(el.find('td:nth-child(3)').text().trim());
-            var percentage = el.find('td:nth-child(4)').text().trim();
+            var abgabe = el.find('td:nth-child(4)').text().trim();
             var arrow = el.find('td:nth-child(1)').text().trim();
-           
-            console.log(beds);
-
-            console.log(percentage);
 
             var elegible = ((el.find(':nth-child(4)').text()
-                .trim().indexOf('10 %') === -1) ? el.find(
+                .trim().indexOf('%') === -1) ? el.find(
                 'td:nth-child(4)').text() : el.find(
                 ':nth-child(5)').text()).trim() === "Ja";
-            if ((beds <= 0) || (!elegible)) {
+
+            if (beds < 1 || !elegible || (!(abgabe === ("10 %") || abgabe === ("0 %")) && !(abgabe === "Ja"))) {
                 el.hide();
             } else {
                 el.show();
             }
 
-            if (arrow === "↓") el.show();
+            if(arrow === "↓") el.show();
         });
     }
 
     var sprechwunsch = $('#h2_sprechwunsch');
-    if (sprechwunsch.length > 0 && sprechwunsch.parent().text().indexOf("Krankenhaus") >= 0) {
+    if (sprechwunsch.length > 0 && sprechwunsch.parent().text().indexOf("Krankenhaus") > 0) {
         filter();
     }
-});
+
+})();
